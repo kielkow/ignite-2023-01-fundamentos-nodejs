a9f7bfad9e29cc370e6ab34f5145db69fb091ffa
kielkow <matheuskiel@fiorifer.com.br>
2023-01-24 15:19:34 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const { method, url, body, params } = req;
 
-    return res.end('server up!')
+    console.log({ method, url, body, params });
+
+    if (method === "GET" && url === "/users") {
+        return res.end('list users');
+    }
+
+    if (method === "POST" && url === "/users") {
+        return res.end('create user');
+    }
+
+    return res.end('server up!');
 });
 
 
