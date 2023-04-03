e16ce7d425138bcaff83ad816d5a88c511e6204e
kielkow <matheuskiel@fiorifer.com.br>
2023-04-03 15:24:00 -0300

    consuming stream completed

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -12,10 +12,22 @@ class TransformToNegativeNumberStream extends Transform {
     }
 }
 
-const server = http.createServer((req, res) => {
-    return req
-        .pipe(new TransformToNegativeNumberStream())
-        .pipe(res);
+const server = http.createServer(async (req, res) => {
+    const buffers = [];
+
+    for await (const chunk of req) {
+        buffers.push(chunk);
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString();
+
+    console.log(fullStreamContent);
+
+    return res.end(fullStreamContent);
+
+    // return req
+    //     .pipe(new TransformToNegativeNumberStream())
+    //     .pipe(res);
 });
 
 server.listen(3334);
