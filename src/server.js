1a4605cdf5245439b99e81f3aedf2af30cd0f16f
kielkow <matheuskiel@fiorifer.com.br>
2023-05-04 11:54:04 -0300

    feat: user uuid

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto';
 
 import { Database } from './database.js';
 import { json } from './middlewares/json.js';
@@ -22,6 +23,7 @@ const server = http.createServer(async (req, res) => {
         const { name, email } = req.body;
 
         const user = {
+            id: randomUUID(),
             name,
             email,
         };
